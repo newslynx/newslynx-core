f9076c7461e90ecdff986309e1d65ae84e66cd84
abelsonlive <brianabelson@gmail.com>
2015-07-20 23:11:02 -0400

    back to square one

--- newslynx/__init__.py
+++ newslynx/__init__.py
@@ -1,12 +1,1 @@
-# run monkey patching here
-import gevent
-from gevent.monkey import patch_all
-patch_all()
-from psycogreen.gevent import patch_psycopg
-patch_psycopg()
-
-# import sous chef
-from .sous_chef import SousChef
-
-# import api object
-from .client import API
+__import__('pkg_resources').declare_namespace(__name__)
