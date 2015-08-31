ee42539b811d65bf41ea73314d26f5c31786f57d
abelsonlive <brianabelson@gmail.com>
2015-08-31 12:12:43 -0400

    fix version command

--- newslynx/cli/version.py
+++ newslynx/cli/version.py
@@ -1,17 +1,19 @@
 """
 Report the version of newslynx.
 """
+import logging 
 
+log = logging.getLogger(__name__)
 
 def setup(parser):
     parser.add_parser("version", help="Report the version.")
     return 'version', run
 
 
-def run(opts, log, **kwargs):
+def run(opts, **kwargs):
     """
     Report the version.
     """
     import pkg_resources
     v = pkg_resources.get_distribution("newslynx").version + "\n"
-    log.info(v, line=False, color='blue')
+    log.info("VERSION: " + v)
